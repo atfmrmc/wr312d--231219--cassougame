26c7fd0e20d9989742ea591087f9f30bd2ed5061
atfmrmc <117905785+atfmrmc@users.noreply.github.com>
2024-01-28 18:47:59 +0100

    cassougame restart

--- src/js/click-drag.js
+++ src/js/click-drag.js
@@ -1,6 +1,15 @@
-import $ from 'jquery';
-
-window.$ = window.jQuery = $;
 $(function () {
-    $(".colored--square").draggable();
+    const grid = $('#colors--grid');
+    let coloredSquares = [];
+
+    $.each(new Array(100), function (i) {
+        const square = $('<div></div>');
+        square.addClass('colored--square');
+        square.css('background-color', 'hsl(' + (i * 360 / 100) + ', 100%, 50%)');
+        square.html(i);
+        coloredSquares.push(square);
+        grid.append(square);
+    });
+
+    
 });
--- src/js/colored-squares-generator.js
+++
@@ -1,9 +0,0 @@
-const grid = document.getElementById('colors--grid');
-let coloredSquares = [];
-
-for (let i = 0; i < 100; i++) {
-    const square = document.createElement('div');
-    square.classList.add('colored--square');
-    grid.appendChild(square);
-    coloredSquares.push(square);
-}
