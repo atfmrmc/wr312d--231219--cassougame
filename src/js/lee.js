ed0f94ad97169fff7beef29cbd216d6181791eec
atfmrmc <117905785+atfmrmc@users.noreply.github.com>
2024-01-29 20:10:26 +0100

    slee cooper

--- src/js/lee.js
+++ src/js/lee.js
@@ -1,13 +1,18 @@
-const leeDiv = $('#leeDiv');
-let lee = $('#lee');
-
 $('#items').sortable({
     connectWith: '#items',
     placeholder: 'placeholder',
 });
 
+$('.section-drag').draggable({
+    handle: '.window-bar',
+    stack: '.section-drag',
+}).resizable({
+    handles: "n, e, s, w"
+});
+
 $('.item').draggable({
     revert: 'invalid',
+    stack: '.section-drag',
     start: function (event, ui) {
         $(ui.helper).addClass('dragging');
     },
@@ -15,18 +20,20 @@ $('.item').draggable({
         $(ui.helper).removeClass('dragging');
     }
 });
-$(leeDiv).droppable({
-        drop: function (event, ui) {
-            if (ui.draggable.hasClass('item')) {
-                ui.draggable.remove();
+$('#leeDiv').droppable({
+    drop: function (event, ui) {
+        if (ui.draggable.hasClass('item')) {
+            ui.draggable.remove();
 
-                // if items has specific class, do something specific
-                if (ui.draggable.hasClass('eyes')) {
-                    lee.attr('src', 'img/lee/lee-eyes.png');
-
-                }
-
+            if (ui.draggable.hasClass('eyes')) {
+                $('#leeDiv').append('<img alt="Eyes" src="/img/lee/lee-eyes.png" class="lee-img lee-eyes"/>');
+            }
+            if (ui.draggable.hasClass('mini')) {
+                $('#leeDiv').append('<img alt="Mini Lee" src="/img/lee/lee-mini.png" class="lee-img lee-mini"/>');
+            }
+            if (ui.draggable.hasClass('slee')) {
+                $('#leeDiv').append('<img alt="Mouth" src="/img/lee/lee-cooper.png" class="lee-img lee-mouth"/>');
             }
         }
     }
-);
+});
